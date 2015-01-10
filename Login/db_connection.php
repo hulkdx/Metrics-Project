237ab5132488f730331bdf64ee7da92885ff9c2c
Tommi <tuominen.tommi.j@student.uta.fi>
2015-01-10 20:04:27 +0200

    Address to real db
    Check login/db_connection.php

--- Login/db_connection.php
+++ Login/db_connection.php
@@ -1,9 +1,15 @@
 <?php
 
-$hostname = "localhost";
-$usrname = "root";
-$password = "";
-$db_name = "metrics";
+/*From email:
+- Server name 'db2.sis.uta.fi'
+- Database name 'metricsmonitoring'
+- User name 'metricsmonitoring'
+- Password 'azHscyDA28KbZQQJ'*/
+
+$hostname = "db2.sis.uta.fi";
+$db_name = "metricsmonitoring";
+$usrname = "metricsmonitoring";
+$password = "azHscyDA28KbZQQJ";
 $con = mysqli_connect($hostname, $usrname, $password, $db_name);
 
 ?>

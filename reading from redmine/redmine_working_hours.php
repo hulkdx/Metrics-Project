f10f01824c65a3110725503c1734a8053eafb5f0
Tommytronic <tommi.tuominen@gmail.com>
2015-01-23 14:37:40 +0200

    removed error checking

--- reading from redmine/redmine_working_hours.php
+++ reading from redmine/redmine_working_hours.php
@@ -9,13 +9,14 @@ Reading Data from Redmine website
  */
 include('getnumbers.php');
 
+/* TODO ERROR CHECKING HERE
 // Difference of these 2 count
 $count_workh = $count_workh - $count_workh_localdb;
 // Check if its negative
 if ($count_workh < 0){
 	// TODO it should be changed.
 	$count_workh = -1 * $count_workh;
-}
+}*/
 
 // individual working hours
 $working_hours = $client->api('time_entry')->all([

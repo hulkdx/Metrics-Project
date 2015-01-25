591b997d4a29152fe089e7bc1bad2ab6ceb3acd5
huangjueran <huangjueran@gmail.com>
2015-01-25 15:33:06 +0200

    sql_file and document

--- metrics(ver.25012015).sql
+++ metrics(ver.25012015).sql
@@ -2,10 +2,10 @@
 -- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- 主机: 127.0.0.1
--- 生成日期: 2015 �?01 �?13 �?17:03
--- 服务器版本: 5.6.11
--- PHP 版本: 5.5.1
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2015 at 02:24 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- 数据库: `metrics`
+-- Database: `metrics`
 --
 CREATE DATABASE IF NOT EXISTS `metrics` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `metrics`;
@@ -25,33 +25,33 @@ USE `metrics`;
 -- --------------------------------------------------------
 
 --
--- 表的结构 `facebook_group`
+-- Table structure for table `facebook_group`
 --
 
 CREATE TABLE IF NOT EXISTS `facebook_group` (
-  `group_id` int(48) NOT NULL AUTO_INCREMENT,
-  `fgroup_id` varchar(48) CHARACTER SET latin1 DEFAULT NULL,
-  `group_name` varchar(48) CHARACTER SET latin1 DEFAULT NULL,
+  `group_id` int(20) NOT NULL AUTO_INCREMENT,
+  `fgroup_id` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `group_name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `facebook_member`
+-- Table structure for table `facebook_member`
 --
 
 CREATE TABLE IF NOT EXISTS `facebook_member` (
-  `member_id` int(48) NOT NULL AUTO_INCREMENT,
-  `member_name` varchar(48) CHARACTER SET latin1 DEFAULT NULL,
-  `fmember_id` varchar(48) CHARACTER SET latin1 DEFAULT NULL,
+  `member_id` int(10) NOT NULL AUTO_INCREMENT,
+  `member_name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `fmember_id` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `individual_work`
+-- Table structure for table `individual_work`
 --
 
 CREATE TABLE IF NOT EXISTS `individual_work` (
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `individual_work` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转存表中的数据 `individual_work`
+-- Dumping data for table `individual_work`
 --
 
 INSERT INTO `individual_work` (`work_id`, `name`, `project_id`, `member_id`, `description`, `hours`, `date`, `issue_id`) VALUES
@@ -865,20 +865,20 @@ INSERT INTO `individual_work` (`work_id`, `name`, `project_id`, `member_id`, `de
 -- --------------------------------------------------------
 
 --
--- 表的结构 `link_table`
+-- Table structure for table `link_table`
 --
 
 CREATE TABLE IF NOT EXISTS `link_table` (
-  `link_id` int(48) NOT NULL AUTO_INCREMENT,
-  `group_id` int(48) DEFAULT NULL,
-  `member_id` int(48) DEFAULT NULL,
+  `link_id` int(20) NOT NULL AUTO_INCREMENT,
+  `group_id` int(20) DEFAULT NULL,
+  `member_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`link_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `member`
+-- Table structure for table `member`
 --
 
 CREATE TABLE IF NOT EXISTS `member` (
@@ -894,7 +894,7 @@ CREATE TABLE IF NOT EXISTS `member` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转存表中的数据 `member`
+-- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`member_id`, `account`, `password`, `first_name`, `last_name`, `level_of_priviledges`, `email`, `phonenumber`) VALUES
@@ -903,13 +903,13 @@ INSERT INTO `member` (`member_id`, `account`, `password`, `first_name`, `last_na
 -- --------------------------------------------------------
 
 --
--- 表的结构 `metrics`
+-- Table structure for table `metrics`
 --
 
 CREATE TABLE IF NOT EXISTS `metrics` (
   `metric_id` int(10) NOT NULL,
-  `metric_name` varchar(10) CHARACTER SET latin1 DEFAULT NULL,
-  `metric_descripition` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
+  `metric_name` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `metric_descripition` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
   `value_type` int(10) DEFAULT NULL,
   PRIMARY KEY (`metric_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -917,7 +917,7 @@ CREATE TABLE IF NOT EXISTS `metrics` (
 -- --------------------------------------------------------
 
 --
--- 表的结构 `metric_values`
+-- Table structure for table `metric_values`
 --
 
 CREATE TABLE IF NOT EXISTS `metric_values` (
@@ -933,7 +933,7 @@ CREATE TABLE IF NOT EXISTS `metric_values` (
 -- --------------------------------------------------------
 
 --
--- 表的结构 `participation`
+-- Table structure for table `participation`
 --
 
 CREATE TABLE IF NOT EXISTS `participation` (
@@ -949,11 +949,11 @@ CREATE TABLE IF NOT EXISTS `participation` (
 -- --------------------------------------------------------
 
 --
--- 表的结构 `project`
+-- Table structure for table `project`
 --
 
 CREATE TABLE IF NOT EXISTS `project` (
-  `project_id` varchar(10) CHARACTER SET latin1 NOT NULL,
+  `project_id` int(10) NOT NULL,
   `project_name` varchar(300) CHARACTER SET latin1 DEFAULT NULL,
   `created_on` datetime DEFAULT NULL,
   `updated_on` datetime DEFAULT NULL,
@@ -964,19 +964,19 @@ CREATE TABLE IF NOT EXISTS `project` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转存表中的数据 `project`
+-- Dumping data for table `project`
 --
 
 INSERT INTO `project` (`project_id`, `project_name`, `created_on`, `updated_on`, `status`, `version`, `discription`) VALUES
-('18', 'Majava', '2012-09-13 07:27:31', '2012-09-13 07:27:31', 0, 0, ''),
-('55555', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 'project desc'),
-('56', 'Metrics Monitoring Tool', '2014-10-04 07:02:50', '2014-10-04 07:02:50', 0, 0, ''),
-('65', 'ampere', NULL, NULL, NULL, NULL, NULL);
+(18, 'Majava', '2012-09-13 07:27:31', '2012-09-13 07:27:31', 0, 0, ''),
+(56, 'Metrics Monitoring Tool', '2014-10-04 07:02:50', '2014-10-04 07:02:50', 0, 0, ''),
+(65, 'ampere', NULL, NULL, NULL, NULL, NULL),
+(55555, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 'project desc');
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `requirement`
+-- Table structure for table `requirement`
 --
 
 CREATE TABLE IF NOT EXISTS `requirement` (
@@ -989,7 +989,7 @@ CREATE TABLE IF NOT EXISTS `requirement` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转存表中的数据 `requirement`
+-- Dumping data for table `requirement`
 --
 
 INSERT INTO `requirement` (`requirement_id`, `project_id`, `description`, `required_working_hours`, `date`) VALUES
@@ -1019,24 +1019,22 @@ INSERT INTO `requirement` (`requirement_id`, `project_id`, `description`, `requi
 -- --------------------------------------------------------
 
 --
--- 表的结构 `weekly_report`
+-- Table structure for table `weekly_report`
 --
 
 CREATE TABLE IF NOT EXISTS `weekly_report` (
-  `report_id` int(100) NOT NULL,
-  `project_id` int(100) DEFAULT NULL,
+  `report_id` int(50) NOT NULL,
+  `project_id` int(10) DEFAULT NULL,
   `number_of_week` int(100) DEFAULT NULL,
-  `project_manager` varchar(2000) CHARACTER SET latin1 DEFAULT NULL,
-  `project_phase` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `project_phase` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
   `completed_tasks` varchar(2000) CHARACTER SET latin1 DEFAULT NULL,
   `task_for_nextweek` varchar(2000) CHARACTER SET latin1 DEFAULT NULL,
   `schedule_status` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
   `next_milestone` varchar(500) CHARACTER SET latin1 DEFAULT NULL,
   `working_hours` varchar(1000) CHARACTER SET latin1 DEFAULT NULL,
-  `requirements` varchar(3000) CHARACTER SET latin1 DEFAULT NULL,
-  `unit_testcases` int(255) DEFAULT NULL,
-  `other_testcases` int(255) DEFAULT NULL,
-  `code_revisions` int(255) DEFAULT NULL,
+  `unit_testcases` int(50) DEFAULT NULL,
+  `other_testcases` int(50) DEFAULT NULL,
+  `code_revisions` int(50) DEFAULT NULL,
   `problems` varchar(500) CHARACTER SET latin1 DEFAULT NULL,
   `changes_in_project_plan` varchar(500) CHARACTER SET latin1 DEFAULT NULL,
   `things_to_mention` varchar(500) CHARACTER SET latin1 DEFAULT NULL,
@@ -1044,21 +1042,50 @@ CREATE TABLE IF NOT EXISTS `weekly_report` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转存表中的数据 `weekly_report`
+-- Dumping data for table `weekly_report`
 --
 
-INSERT INTO `weekly_report` (`report_id`, `project_id`, `number_of_week`, `project_manager`, `project_phase`, `completed_tasks`, `task_for_nextweek`, `schedule_status`, `next_milestone`, `working_hours`, `requirements`, `unit_testcases`, `other_testcases`, `code_revisions`, `problems`, `changes_in_project_plan`, `things_to_mention`) VALUES
-(34, 12, 4, '', '', 'x', 'x', 'x', 'x', '35', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(55, 34, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(98, 98, 4, '', '', 'x', 'x', 'x', 'x', '35', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(1234, 4321, 45, 'qiasm', NULL, '5', '5', '5', NULL, NULL, NULL, 31, 43, 53, NULL, NULL, NULL),
-(5589, 6666, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(6456, 564645, 4, '', '', 'x', 'x', 'x', 'x', '35', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(7575, 6757, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(12345, 1234, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(66666, 666666, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(555555, 55555, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x'),
-(2147483647, 5555, 4, '', '', 'x', 'x', 'x', 'x', '0', 'x', 0, 0, 0, 'x', 'x', 'x');
+INSERT INTO `weekly_report` (`report_id`, `project_id`, `number_of_week`, `project_phase`, `completed_tasks`, `task_for_nextweek`, `schedule_status`, `next_milestone`, `working_hours`, `unit_testcases`, `other_testcases`, `code_revisions`, `problems`, `changes_in_project_plan`, `things_to_mention`) VALUES
+(34, 12, 4, '', 'x', 'x', 'x', 'x', '35', 0, 0, 0, 'x', 'x', 'x'),
+(55, 34, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x'),
+(98, 98, 4, '', 'x', 'x', 'x', 'x', '35', 0, 0, 0, 'x', 'x', 'x'),
+(1234, 4321, 45, NULL, '5', '5', '5', NULL, NULL, 31, 43, 53, NULL, NULL, NULL),
+(5589, 6666, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x'),
+(6456, 564645, 4, '', 'x', 'x', 'x', 'x', '35', 0, 0, 0, 'x', 'x', 'x'),
+(7575, 6757, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x'),
+(12345, 1234, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x'),
+(66666, 666666, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x'),
+(555555, 55555, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x'),
+(2147483647, 5555, 4, '', 'x', 'x', 'x', 'x', '0', 0, 0, 0, 'x', 'x', 'x');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weekly_report_manager`
+--
+
+CREATE TABLE IF NOT EXISTS `weekly_report_manager` (
+  `manager_id` int(50) NOT NULL AUTO_INCREMENT,
+  `project_id` int(50) NOT NULL,
+  `weekly_report_id` int(50) NOT NULL,
+  `manager_name` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `manager_email` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`manager_id`,`project_id`,`weekly_report_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weekly_report_requirement`
+--
+
+CREATE TABLE IF NOT EXISTS `weekly_report_requirement` (
+  `project_id` int(50) DEFAULT NULL,
+  `weekly_report_id` int(50) DEFAULT NULL,
+  `requirement_id` int(50) DEFAULT NULL,
+  `requirement_name` varchar(60) CHARACTER SET latin1 DEFAULT NULL,
+  `requirement_status` varchar(50) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
